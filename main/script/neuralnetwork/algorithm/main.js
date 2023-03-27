a30f8512ecadf14db3e643804e43f605382bc4b8
kpullela10 <kpullela10@gmail.com>
2023-03-27 10:44:56 -0700

    neural network doesn't work yet

--- main/script/neuralnetwork/algorithm/main.js
+++ main/script/neuralnetwork/algorithm/main.js
@@ -1,45 +1,59 @@
-function classifyImage() {
-  console.log("checkpoint 1");
-  var imageInput = document.getElementById("imageInput");
-  var file = imageInput.files[0];
-
-  var reader = new FileReader();
-  reader.onload = function (event) {
-    var img = new Image();
-    img.onload = function () {
-      predictImage(img);
-    };
-    img.src = event.target.result;
-  };
-  reader.readAsDataURL(file);
-}
-
-async function predictImage(img) {
-  const model = await tf.loadLayersModel(
-    "https://storage.googleapis.com/tfjs-models/tfjs/cifar10_v1/model.json"
+async function fetchLabelNames() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/weiaicunzai/pytorch-cifar100/master/labels.txt"
   );
-  const tensor = tf.browser
-    .fromPixels(img)
-    .resizeNearestNeighbor([32, 32])
-    .expandDims()
-    .toFloat()
-    .div(tf.scalar(255));
-  const predictions = await model.predict(tensor).data();
-  const classId = predictions.indexOf(Math.max(...predictions));
-  const classNames = [
-    "airplane",
-    "automobile",
-    "bird",
-    "cat",
-    "deer",
-    "dog",
-    "frog",
-    "horse",
-    "ship",
-    "truck",
-  ];
-  const resultElement = document.getElementById("result");
-  resultElement.innerText = `Prediction: ${classNames[classId]} (${(
-    predictions[classId] * 100
-  ).toFixed(2)}%)`;
+  const text = await response.text();
+  const labelNames = text.split("\n").filter((name) => name !== "");
+  return labelNames;
 }
+
+async function loadImage(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
+  });
+}
+
+async function main() {
+  // Load the pre-trained model
+  const model = torchvision.models.resnet18({ pretrained: true });
+  model.eval();
+
+  // Get the label names for CIFAR100
+  const labelNames = await fetchLabelNames();
+
+  // Get the input image from the user
+  const fileInput = document.getElementById("file-input");
+  fileInput.addEventListener("change", async () => {
+    const file = fileInput.files[0];
+    const img = await loadImage(file);
+
+    // Preprocess the input image
+    const tensor = torchvision.transforms.ToTensor()(img);
+    const normalized = torchvision.transforms.Normalize(
+      [0.5071, 0.4865, 0.4409],
+      [0.2673, 0.2564, 0.2762]
+    )(tensor);
+    const batched = normalized.unsqueeze(0);
+
+    // Run the model inference
+    const output = model.forward(batched);
+
+    // Get the predicted class index
+    const values = output.detach().squeeze().tolist();
+    const index = values.indexOf(Math.max(...values));
+
+    // Show the predicted class label
+    const predictedLabel = labelNames[index];
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = predictedLabel;
+  });
+}
+
+// Call the main function to start the program
+main();
